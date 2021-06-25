87bb2d1cca4d1e5b0049043cddf12c1eb51425a0
Masud Rana <masudranawd2019@gmail.com>
2021-06-25 07:14:05 +0600

    novoair-server

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbld0.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const ticketCollection = client.db("novoair").collection("ticketDetail");
+    
+    // add new ticket
+    app.post('/addTicket', (req, res) => {
+        const ticket = req.body;
+        ticketCollection.insertOne(ticket)
+        .then(result => {
+            res.send(result.insertedCount)
+        })
+    })
+
+    // all ticket List
+    app.get('/ticketList', (req, res) => {
+        ticketCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+
+    // // single ticket
+    // app.get('/service/:id', (req, res) => {
+    //     servicesCollection.find({ _id: ObjectId(req.params.id) })
+    //     .toArray((err, documents) => {
+    //         res.send(documents);
+    //     })
+    // })
+
+     
+
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server Working...')
+})
+app.listen(process.env.PORT || port);
